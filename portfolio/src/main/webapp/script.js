7b725ce746c043ca9872dc7db037f1ba5d2ec082
Yichen <yicyao@gmail.com>
2020-06-24 21:53:53 +0000

    slideshow now has mini slideshow, add mini slideshow images

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,12 +14,11 @@
 
 /* Creates Slideshow Carousel to go through my projects*/
 
-var slideIndex = 1;
-showSlides("arrow", slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides("arrow", slideIndex += n);
+
 }
 
 // Thumbnail image controls
@@ -28,19 +27,26 @@ function currentSlide(n) {
 }
 
 function showSlides(type, n) {
-  var i;
-  if (type === "dots") { var slides = document.getElementsByClassName("miniSlides");}
-  else {var slides = document.getElementsByClassName("mySlides");}
-  
-  var dots = document.getElementsByClassName("dot");
+  let slides;
+  if (type === "dots") { 
+    slides = document.getElementsByClassName("miniSlides");
+  }
+  else {
+    slides = document.getElementsByClassName("mySlides");
+  }
+
+  const dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  dots[slideIndex-1].className += " active";
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
