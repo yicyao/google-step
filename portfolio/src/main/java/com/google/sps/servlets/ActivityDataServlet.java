8241bf0a06ce4e8d99dbb83fce0b0318e69812a2
Yichen <yicyao@gmail.com>
2020-07-08 21:04:50 +0000

    store activity votes in database

--- portfolio/src/main/java/com/google/sps/servlets/ActivityDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ActivityDataServlet.java
@@ -1,5 +1,13 @@
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,20 +20,46 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/activity-data")
 public class ActivityDataServlet extends HttpServlet {
-  private Map<String, Integer> activityVotes = new HashMap<>();
+  private static final String ACTIVITY_OBJ = "Activity";
+  private static final String ACTIVITY = "activity";
+  private static final String ACTIVITY_COUNT = "count";
+  
 
+  // write javadoc
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    PreparedQuery results = datastore.prepare(new Query(ACTIVITY_OBJ));
+    Map<String, Long> activityVotes = new HashMap<>();
+    for (Entity entity : results.asIterable()) {
+      String activityName = (String) entity.getProperty(ACTIVITY);
+      long count = (long) entity.getProperty(ACTIVITY_COUNT);
+
+      if (activityVotes.containsKey(activityName)) {
+        activityVotes.put(activityName, activityVotes.get(activityName) + count);
+      } else {
+        activityVotes.put(activityName, count);
+      }
+    }
+
     response.setContentType("application/json");
     response.getWriter().println(new Gson().toJson(activityVotes));
   }
 
+  /** Takes single activity input and writes into database*/
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String activity = request.getParameter("activity");
-    int currentVotes = activityVotes.containsKey(activity) ? activityVotes.get(activity) : 0;
-    activityVotes.put(activity, currentVotes + 1);
-
+    writeToDatastore(request.getParameter(ACTIVITY));
     response.sendRedirect("/fun-features.html");
+  }
+
+  private void writeToDatastore(String activity) {
+    Entity activityEntity = new Entity(ACTIVITY_OBJ);
+    activityEntity.setProperty(ACTIVITY, activity);
+    activityEntity.setProperty(ACTIVITY_COUNT, 1);
+    
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(activityEntity);
   }
 }
