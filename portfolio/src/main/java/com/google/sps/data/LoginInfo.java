f2f8b6230cedfa5d63127388856f8ee3f412d8f4
Yichen <yicyao@gmail.com>
2020-07-10 03:43:12 +0000

    make LoginInfo vars private

--- portfolio/src/main/java/com/google/sps/data/LoginInfo.java
+++ portfolio/src/main/java/com/google/sps/data/LoginInfo.java
@@ -3,8 +3,8 @@ package com.google.sps.data;
 /** Class containing user login info */
 public final class LoginInfo {
   /** Log-in link if user is logged out, log-out link if the user is logged in.*/
-  public final Boolean loggedIn;
-  public final String link;
+  private final Boolean loggedIn;
+  private final String link;
 
   public LoginInfo(Boolean loggedIn, String link) {
     this.loggedIn = loggedIn;
