f3b01511332b4819d1cc3f4d1ffd024366bfc6bd
Yichen <yicyao@gmail.com>
2020-07-13 23:17:21 +0000

    query function to find all possible meeting times

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,73 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+/** Finds possible meeting times for a request for all mandatory participants */
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+
+    /**
+    * @param events List of all known events with times and participants
+    * @param request Meeting to be planned
+    * @return List of all possible meeting times
+    */
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        List<TimeRange> busyTimes = getBusyTimes(events, request);
+        Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
+        busyTimes = mergeIntervals(busyTimes);
+        return getAvailableTimes(busyTimes, request.getDuration());
+    }
+
+    // gets busy times for all request attendees
+    private ArrayList<TimeRange> getBusyTimes(Collection<Event> events, MeetingRequest request) {
+        ArrayList<TimeRange> busyTimes = new ArrayList<>();
+        for (Event event: events) {
+            for (String attendees: request.getAttendees()) {
+                if (event.getAttendees().contains(attendees)) {
+                    busyTimes.add(event.getWhen());
+                }
+            }
+        }
+        
+        return busyTimes;
+    }
+
+    // merges overlapping intervals
+    private List<TimeRange> mergeIntervals(List<TimeRange> intervals) {
+    List<TimeRange> result = new ArrayList<>();
+    for (int i = 0; i < intervals.size(); i++) {
+        TimeRange currentRange = intervals.get(i);
+        while (i + 1 < intervals.size() && currentRange.overlaps(intervals.get(i + 1))) {
+        // Combine the two overlapping Timeintervals into a single TimeRange
+        currentRange = TimeRange.fromStartEnd(currentRange.start(),
+            Math.max(currentRange.end(), intervals.get(i + 1).end()),
+            /* inclusive=*/false);
+        i++;
+        }
+        result.add(currentRange);
+    }
+    return result;
+    }
+
+    //gets available times of correct duration
+    private List<TimeRange> getAvailableTimes(List<TimeRange> busyTimes, long duration){
+    List<TimeRange> availableTimes = new ArrayList<>();
+
+    int currentStart = TimeRange.START_OF_DAY;
+    for (TimeRange time: busyTimes) {
+        if (time.start() - currentStart >= duration) {
+            availableTimes.add(TimeRange.fromStartEnd(currentStart, time.start(), /* inclusive=*/ false));
+        }
+        currentStart = time.end();
+    }
+
+    if (TimeRange.END_OF_DAY - currentStart >= duration) {
+        availableTimes.add(TimeRange.fromStartEnd(currentStart, TimeRange.END_OF_DAY, /* inclusive=*/ true));
+    }
+    return availableTimes;
+    }
+  
 }
