25b7573c2758cb9bb2feea3c8b2154400fd2d7ee
Yichen <yicyao@gmail.com>
2020-07-14 00:17:44 +0000

    make mergeIntervals more effecient

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -21,66 +21,73 @@ import java.util.List;
 
 /** Finds possible meeting times for a request for all mandatory participants */
 public final class FindMeetingQuery {
+  /**
+   * @param events List of all known events with times and participants
+   * @param request Meeting to be planned
+   * @return List of all possible meeting times
+   */
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    List<TimeRange> busyTimes = getBusyTimes(events, request);
+    Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
+    busyTimes = mergeIntervals(busyTimes);
+    return getAvailableTimes(busyTimes, request.getDuration());
+  }
 
-    /**
-    * @param events List of all known events with times and participants
-    * @param request Meeting to be planned
-    * @return List of all possible meeting times
-    */
-    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-        List<TimeRange> busyTimes = getBusyTimes(events, request);
-        Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
-        busyTimes = mergeIntervals(busyTimes);
-        return getAvailableTimes(busyTimes, request.getDuration());
-    }
-
-    // gets busy times for all request attendees
-    private ArrayList<TimeRange> getBusyTimes(Collection<Event> events, MeetingRequest request) {
-        ArrayList<TimeRange> busyTimes = new ArrayList<>();
-        for (Event event: events) {
-            for (String attendees: request.getAttendees()) {
-                if (event.getAttendees().contains(attendees)) {
-                    busyTimes.add(event.getWhen());
-                }
-            }
+  // gets busy times for all request attendees
+  private ArrayList<TimeRange> getBusyTimes(Collection<Event> events, MeetingRequest request) {
+    ArrayList<TimeRange> busyTimes = new ArrayList<>();
+    for (Event event : events) {
+      for (String attendees : request.getAttendees()) {
+        if (event.getAttendees().contains(attendees)) {
+          busyTimes.add(event.getWhen());
         }
-        
-        return busyTimes;
+      }
+    }
+    return busyTimes;
+  }
+
+  // merges intervals of overlapping events
+  private List<TimeRange> mergeIntervals(List<TimeRange> intervals) {
+    if (intervals == null || intervals.size() <= 1) {
+      return intervals;
     }
 
-    // merges overlapping intervals
-    private List<TimeRange> mergeIntervals(List<TimeRange> intervals) {
     List<TimeRange> result = new ArrayList<>();
-    for (int i = 0; i < intervals.size(); i++) {
-        TimeRange currentRange = intervals.get(i);
-        while (i + 1 < intervals.size() && currentRange.overlaps(intervals.get(i + 1))) {
-        // Combine the two overlapping Timeintervals into a single TimeRange
-        currentRange = TimeRange.fromStartEnd(currentRange.start(),
-            Math.max(currentRange.end(), intervals.get(i + 1).end()),
-            /* inclusive=*/false);
-        i++;
-        }
-        result.add(currentRange);
+    TimeRange startInterval = intervals.get(0);
+    for (int i = 1; i < intervals.size(); i++) {
+      TimeRange currentInterval = intervals.get(i);
+      // if events overlap, merge
+      if (currentInterval.start() <= startInterval.end()) {
+        startInterval = TimeRange.fromStartEnd(startInterval.start(),
+            Math.max(currentInterval.end(), startInterval.end()), /* inclusive=*/false);
+      } else {
+        result.add(startInterval);
+        startInterval = currentInterval;
+      }
     }
+    result.add(startInterval);
     return result;
-    }
+  }
 
-    //gets available times of correct duration
-    private List<TimeRange> getAvailableTimes(List<TimeRange> busyTimes, long duration){
+  // gets available times of correct duration
+  private List<TimeRange> getAvailableTimes(List<TimeRange> busyTimes, long duration) {
     List<TimeRange> availableTimes = new ArrayList<>();
 
+    // finds gaps of requested duration time
     int currentStart = TimeRange.START_OF_DAY;
-    for (TimeRange time: busyTimes) {
-        if (time.start() - currentStart >= duration) {
-            availableTimes.add(TimeRange.fromStartEnd(currentStart, time.start(), /* inclusive=*/ false));
-        }
-        currentStart = time.end();
+    for (TimeRange time : busyTimes) {
+      if (time.start() - currentStart >= duration) {
+        availableTimes.add(
+            TimeRange.fromStartEnd(currentStart, time.start(), /* inclusive=*/false));
+      }
+      currentStart = time.end();
     }
 
+    // finds gap of requested duration time between last meeting and end of day
     if (TimeRange.END_OF_DAY - currentStart >= duration) {
-        availableTimes.add(TimeRange.fromStartEnd(currentStart, TimeRange.END_OF_DAY, /* inclusive=*/ true));
+      availableTimes.add(
+          TimeRange.fromStartEnd(currentStart, TimeRange.END_OF_DAY, /* inclusive=*/true));
     }
     return availableTimes;
-    }
-  
+  }
 }
